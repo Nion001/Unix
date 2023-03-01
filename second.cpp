4eb51bf844c8d755e53ced54926f768ec84bd9de
Nion001 <90500568+Nion001@users.noreply.github.com>
2023-03-01 20:14:57 +0700

    Задание 3
    открыть файл и записать данные

--- second.cpp
+++ second.cpp
@@ -2,13 +2,26 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <string.h>
+
 
 using namespace std;
 int main ()
 {
+ unsigned int fd;
+ char  *buffer ="Привет,это данные для записи в файл";
+ unsigned int count;
+ int nwritten;
 
-int fd=open("second",O_RDONLY);
-cout<<"Моя вторая программа, файловый дискриптор="<<" "<<fd<<endl;
-return 0;
+ count = strlen(buffer);
+ fd = open("data.txt", O_WRONLY);
+   if (fd==-1)
+      {
+      fputs("Не могу открыть файл",stdout);
+      return;
+      }
+nwritten = write (fd, buffer, count);
+printf ("\n WRITE:\n%u байтов записано в файл\n",nwritten);
+close(fd);
 }
 
